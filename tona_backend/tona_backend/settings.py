855e0bfab84cd6a1e3e2e67cb39fa03ad7c5a8d0
HauleTZ <williamhaule326@gmail.com>
2022-12-29 20:57:18 +0300

    setting.py updated

--- tona_backend/tona_backend/settings.py
+++ tona_backend/tona_backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2^kd%j$xxv!hhd35_h^#hp215_ohdd2luc7$+gkzcc0@ak@)#x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
