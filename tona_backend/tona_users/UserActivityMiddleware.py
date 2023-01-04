618140aa5ad962e914f60468a5c581e27314a15a
kulwa-silya <kulwamalyango@gmail.com>
2023-01-04 07:08:08 +0300

    Usertracker optmization 1, adding tokens to cache, next optmiziation is toimplement celery to handle data saving to db

--- tona_backend/tona_users/UserActivityMiddleware.py
+++ tona_backend/tona_users/UserActivityMiddleware.py
@@ -1,8 +1,30 @@
 from .models import UserActivity
 from django.utils import timezone
 from django.contrib.auth import get_user_model
+from django.core.cache import cache
 
 import jwt
+
+def cleanPatchData(data):
+    lines = data.strip().split('\n')
+
+    # Initialize an empty dictionary to store the name/value pairs
+    parsed_data = {}
+
+    # Iterate through the lines
+    for i, line in enumerate(lines):
+        # Check if the line starts with "Content-Disposition"
+        if line.startswith('Content-Disposition'):
+            # Extract the name from the line
+            name = line.split(';')[-1].split('=')[-1].strip('"').rstrip('\r')
+            # Get the value from the next line
+            value = lines[i+2].rstrip('\r')
+            # Store the name/value pair in the dictionary
+            parsed_data[name] = value
+
+    # Print the parsed data
+    return parsed_data
+
 
 class UserActivityMiddleware:
     def __init__(self, get_response):
@@ -12,8 +34,14 @@ class UserActivityMiddleware:
         # Code to be executed for each request before the view (and later middleware) are called.
         if request.META.get('HTTP_AUTHORIZATION', None):
         
+            # Try to get the decoded token from the cache to avoid the overhead of decoding the token on every request.
             TOKEN = request.META.get('HTTP_AUTHORIZATION', None).split()[-1]
-            TOKEN_DICT = jwt.decode(TOKEN, options={"verify_signature": False})
+            TOKEN_DICT = cache.get(TOKEN)
+            if TOKEN_DICT is None:
+                # If the token is not in the cache, decode it and store it in the cache
+                TOKEN_DICT = jwt.decode(TOKEN, options={"verify_signature": False})
+                cache.set(TOKEN, TOKEN_DICT)
+            print(cache)
 
             User = get_user_model()
             user = User.objects.get(id=TOKEN_DICT['user_id'])
@@ -26,7 +54,10 @@ class UserActivityMiddleware:
             tracked_groups = ['sales','deliver']
 
             # clean querydict post data
-            data = {k: v for k, v in request.POST.dict().items() if k != 'csrfmiddlewaretoken'}
+            if request.method == 'POST':
+                data = {k: v for k, v in request.POST.dict().items() if k != 'csrfmiddlewaretoken'}
+            else:
+                data = cleanPatchData(request.body.decode('utf-8'))
 
             # Log the user's activity
             if TOKEN_DICT['token_type'] == 'access' and request.method != 'GET' and any(g == user_group.name for g in tracked_groups):
