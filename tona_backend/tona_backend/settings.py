8b4c44b8eae4895d8c5461df8914dea83a2f9052
HauleTZ <williamhaule326@gmail.com>
2023-02-03 15:52:53 +0300

    whitenoise and staticfile dir added

--- tona_backend/tona_backend/settings.py
+++ tona_backend/tona_backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2^kd%j$xxv!hhd35_h^#hp215_ohdd2luc7$+gkzcc0@ak@)#x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    #"whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -163,3 +164,14 @@ DJOSER = {
 CSRF_TRUSTED_ORIGINS = ['https://tona-production.up.railway.app/admin']
 
 # PHONENUMBER_DEFAULT_REGION = "TZ"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
+
+MEDIA_URL='/images/'
+
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
