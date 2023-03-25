3e76db3bd675c4bf9e369f06dd65fc613ea56397
HauleTZ <williamhaule326@gmail.com>
2023-03-25 10:55:37 +0300

    Purchased product model modified

--- tona_backend/procurement/models.py
+++ tona_backend/procurement/models.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.db import models
 # from datetime import date
 from store.models import Product
+from store.validators import validate_file_size
+from django.core.validators import MinValueValidator
 
 
 # Create your models here.
@@ -20,15 +22,33 @@ class PurchasedProduct(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='productPurchased')
     date = models.DateField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
     supplier = models.ForeignKey(Supplier, related_name='supplier', on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        # update inventory
+        self.product.inventory += self.quantity
+        self.product.save()
+
 
 
 class Purchase(models.Model):
     date = models.DateField()
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=255)
     purchased_products = models.ManyToManyField(PurchasedProduct, related_name='purchasedproduct')
     # include receipts
+    
+
+#single purchase can have more than one receipts
+class Receipt(models.Model):
+    purchase = models.ForeignKey(
+        Purchase, on_delete=models.CASCADE, related_name='receipt')
+    image = models.ImageField(
+        upload_to='store/receipts',
+        validators=[validate_file_size])
 
 
 class AssociatedCost(models.Model):
