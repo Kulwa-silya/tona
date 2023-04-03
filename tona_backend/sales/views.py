e9d3edda0be8e4075e696594e572a540281f15e5
Kulwa-silya <kulwamalyango@gmail.com>
2023-04-03 19:37:33 +0300

    removed  unique constraint in customer phone_number field in sale

--- tona_backend/sales/views.py
+++ tona_backend/sales/views.py
@@ -1,15 +1,9 @@
-from store.permissions import FullDjangoModelPermissions, IsAdminOrReadOnly, ViewCustomerHistoryPermission
-from store.pagination import DefaultPagination
-from django.db.models.aggregates import Count
-from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 # from django.contrib.auth.mixins import UserPassesTestMixin
-from rest_framework.decorators import action, permission_classes
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.permissions import AllowAny, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from .filters import SoldProductFilter, SaleFilter
 from .models import *
